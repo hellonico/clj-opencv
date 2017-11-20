6fc6ae367d1036e6d7b826bf3fca609429784e5b
Nicolas Modrzyk <hellonico@gmail.com>
2017-11-20 14:58:28 +0900

    INIT

---
+++ src/leiningen/new/clj_opencv/project.clj
@@ -0,0 +1,29 @@
+(defproject {{ns-name}} "0.1-SNAPSHOT"
+  :injections [
+   (clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)
+  ]
+  :plugins [[lein-gorilla "0.4.0"]]
+  :test-paths ["test" "samples"]
+  :resource-paths ["rsc"]
+  :main opencv3.first
+  :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
+  :aliases {"notebook" ["gorilla" ":ip" "0.0.0.0" ":port" "10000"]}
+  
+  :profiles {:dev {
+    :resource-paths ["resources"]
+    :dependencies [
+    ; used for proto repl
+    [org.clojure/tools.nrepl "0.2.11"]
+    ; proto repl
+    [proto-repl "0.3.1"]
+    ; use to start a gorilla repl
+    [gorilla-repl "0.4.0"]
+    [seesaw "1.4.5"]
+
+     ]}}
+  :dependencies [
+   [org.clojure/clojure "1.8.0"]
+   [org.clojure/tools.cli "0.3.5"]
+   [origami "0.1.2"]
+
+])
