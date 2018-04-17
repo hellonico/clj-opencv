0be596e06ba92f86303cc3d3dbaa7f7243ab2b13
Nicolas Modrzyk <nico@karabiner-software.com>
2018-04-17 10:32:48 +0900

    Version 0.0.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-opencv/lein-template "0.0.10-SNAPSHOT"
+(defproject clj-opencv/lein-template "0.0.10"
   :description "Created with lein-create-template"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
