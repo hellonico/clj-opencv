36827b9a9b5522c4ce70229fcc1b6b75fad87721
Nicolas Modrzyk <hellonico@gmail.com>
2018-04-17 14:51:22 +0900

    deploy to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-opencv/lein-template "0.0.10"
   :description "Created with lein-create-template"
   :url "http://example.com/FIXME"
+  :deploy-repositories [["releases" :clojars]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
