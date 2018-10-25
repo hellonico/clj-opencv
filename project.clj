162e20222c65264f70c23dfcd78a3373b83c76e1
Nicolas Modrzyk <nico@karabiner-software.com>
2018-10-25 14:57:59 +0900

    Version 0.0.14-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-opencv/lein-template "0.0.13"
+(defproject clj-opencv/lein-template "0.0.14-SNAPSHOT"
   :description "Created with lein-create-template"
   :url "http://example.com/FIXME"
   :deploy-repositories [["releases" :clojars]]
