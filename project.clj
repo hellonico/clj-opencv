f37960520b31a432e61dc9a63b40a58ae66bfe9e
Nicolas Modrzyk <hellonico@gmail.com>
2019-01-16 16:56:35 +0900

    Version 0.0.26-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-opencv/lein-template "0.0.25"
+(defproject clj-opencv/lein-template "0.0.26-SNAPSHOT"
   :description "Created with lein-create-template"
   :url "http://example.com/FIXME"
   :deploy-repositories [["releases" :clojars]]
