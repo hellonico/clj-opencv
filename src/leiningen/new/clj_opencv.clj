3fdbdebe2c7fcb2ab4fa9e77ab7625c665bbb9ca
Nicolas Modrzyk <hellonico@gmail.com>
2020-05-25 15:00:04 +0900

    sources and filters

--- src/leiningen/new/clj_opencv.clj
+++ src/leiningen/new/clj_opencv.clj
@@ -8,51 +8,49 @@
   [name]
   (let [data {:name name
               :ns-name (sanitize-ns name)
-              :version "4.3.0-7"
+              :version "4.3.0-8"
               :sanitized (name-to-path name)}]
-    (->files data 
+    (->files data
 
 ; project
-["project.clj" (render "project.clj" data)]
-["deps.edn" (render "deps.edn" data)]
-["README.md" (render "README.md" data)]
-["Dockerfile" (render "Dockerfile" data)]
+             ["project.clj" (render "project.clj" data)]
+             ["deps.edn" (render "deps.edn" data)]
+             ["README.md" (render "README.md" data)]
+             ["Dockerfile" (render "Dockerfile" data)]
 
 ; notes
 ; ["notes/practice.clj" (render "notes/practice.clj" data)]
 ; ["notes/empty.clj" (render "notes/empty.clj" data)]
 
 ; jupyter
-["jupyter/firstbook.ipynb" (render "jupyter/firstbook.ipynb" data)]
+             ["jupyter/firstbook.ipynb" (render "jupyter/firstbook.ipynb" data)]
 
 ; output folder
-["output/.gitkeep" (render "output/gitkeep" data)]
+             ["output/.gitkeep" (render "output/gitkeep" data)]
 
 
 ; samples
-["src/opencv4/ok.clj" (render "src/opencv4/ok.clj" data)]
-["src/opencv4/simple.clj" (render "src/opencv4/simple.clj" data)]
-["src/opencv4/tutorial.clj" (render "src/opencv4/tutorial.clj" data)]
-["src/opencv4/videosample.clj" (render "src/opencv4/videosample.clj" data)]
-["src/opencv4/lena.clj" (render "src/opencv4/lena.clj" data)]
+             ["src/opencv4/ok.clj" (render "src/opencv4/ok.clj" data)]
+             ["src/opencv4/simple.clj" (render "src/opencv4/simple.clj" data)]
+             ["src/opencv4/tutorial.clj" (render "src/opencv4/tutorial.clj" data)]
+             ["src/opencv4/videosample.clj" (render "src/opencv4/videosample.clj" data)]
+             ["src/opencv4/lena.clj" (render "src/opencv4/lena.clj" data)]
 
 ; images
-["resources/cat.jpg" (raw "resources/cat.jpg")]
-["resources/minicat.jpg" (raw "resources/minicat.jpg")]
-["resources/nekobench.jpg" (raw "resources/nekobench.jpg")]
-["resources/souslesoleil.jpg" (raw "resources/souslesoleil.jpg")]
-["resources/sunflower.jpg" (raw "resources/sunflower.jpg")]
+             ["resources/cat.jpg" (raw "resources/cat.jpg")]
+             ["resources/minicat.jpg" (raw "resources/minicat.jpg")]
+             ["resources/nekobench.jpg" (raw "resources/nekobench.jpg")]
+             ["resources/souslesoleil.jpg" (raw "resources/souslesoleil.jpg")]
+             ["resources/sunflower.jpg" (raw "resources/sunflower.jpg")]
 
-["resources/lena.png" (raw "resources/lena.png")]
-["resources/blurred.png" (raw "resources/blurred.png")]
+             ["resources/lena.png" (raw "resources/lena.png")]
+             ["resources/blurred.png" (raw "resources/blurred.png")]
 
 ; setting files
-["resources/XML/aGest.xml" (render "resources/XML/aGest.xml")]
-["resources/XML/palm.xml" (render "resources/XML/palm.xml")]
-["resources/XML/face.xml" (render "resources/XML/face.xml")]
-["resources/XML/closed_frontal_palm.xml" (render "resources/XML/closed_frontal_palm.xml")]
-["resources/XML/fist.xml" (render "resources/XML/fist.xml")]
-["resources/XML/haarcascade_eye_tree_eyeglasses.xml" (render "resources/XML/haarcascade_eye_tree_eyeglasses.xml")]
-["resources/XML/haarcascade_frontalface_alt2.xml" (render "resources/XML/haarcascade_frontalface_alt2.xml")]
-
-)))
+             ["resources/XML/aGest.xml" (render "resources/XML/aGest.xml")]
+             ["resources/XML/palm.xml" (render "resources/XML/palm.xml")]
+             ["resources/XML/face.xml" (render "resources/XML/face.xml")]
+             ["resources/XML/closed_frontal_palm.xml" (render "resources/XML/closed_frontal_palm.xml")]
+             ["resources/XML/fist.xml" (render "resources/XML/fist.xml")]
+             ["resources/XML/haarcascade_eye_tree_eyeglasses.xml" (render "resources/XML/haarcascade_eye_tree_eyeglasses.xml")]
+             ["resources/XML/haarcascade_frontalface_alt2.xml" (render "resources/XML/haarcascade_frontalface_alt2.xml")])))
--- src/leiningen/new/clj_opencv/project.clj
+++ src/leiningen/new/clj_opencv/project.clj
@@ -1,35 +1,28 @@
 (defproject {{ns-name}} "0.1-SNAPSHOT"
-:test-paths ["test"]
-:plugins [
-;[hellonico/lein-gorilla "0.4.2"]
-[lein-auto "0.1.3"][lein-jupyter "0.1.16"]]
-:auto {:default {:file-pattern #"\.(clj)$"}}
-:main opencv4.ok
-:repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
+  :test-paths ["test"]
+  :plugins [;[hellonico/lein-gorilla "0.4.2"]
+            [lein-auto "0.1.3"] [lein-jupyter "0.1.16"]]
+  :auto {:default {:file-pattern #"\.(clj)$"}}
+  :main opencv4.ok
+  :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
 ; :aliases {"notebook" ["gorilla" ":ip" "0.0.0.0" ":port" "10000"]}
-:profiles {:dev {
-  :resource-paths ["resources"]
-  :dependencies [
-  ; used for proto repl
+  :profiles {:dev {:resource-paths ["resources"]
+                   :dependencies [; used for proto repl
   ; [org.clojure/tools.nrepl "0.2.11"]
   ; proto repl
   ; [proto-repl "0.3.1"]
   ; use to start a gorilla repl
   ; [hellonico/gorilla-repl "0.4.1"]
-  [seesaw "1.4.5"]
-  ]
-  }}
-:dependencies [
- [org.clojure/clojure "1.9.0"]
- [org.clojure/tools.cli "0.3.5"]
- [origami "{{version}}"]
- [origami/filters "1.11"]
- [origami/youtube "1..0.0"]
+                                  [seesaw "1.4.5"]]}}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [origami "{{version}}"]
+                 [origami/filters "1.11"]
+                 [origami-sources "1.0.0"]
 
  ; uncomment to use only the binary for your platform
  ;[origami "{{version}}" :exclusions [opencv/opencv-native]]
  ;[opencv/opencv-native "{{version}}" :classifier "osx_64"]
  ;[opencv/opencv-native "{{version}}" :classifier "linux_64"]
  ;[opencv/opencv-native "{{version}}" :classifier "windows_64"]
-
- ])
+                 ])
